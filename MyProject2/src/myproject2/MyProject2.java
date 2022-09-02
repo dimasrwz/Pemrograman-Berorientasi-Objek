8e2fce0b1f81e360161df673d99047a57d1cc940
dimasr <89757844+dimasrwz@users.noreply.github.com>
2022-09-02 09:06:23 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author Bagus C
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("DDDDDD            IIIII        MM            MM");
+        System.out.println("DDDDDDDD          IIIII        MM  MM    MM  MM");
+        System.out.println("DDD    DD         IIIII        MM     MM     MM");
+        System.out.println("DDD    DD         IIIII        MM     MM     MM");
+        System.out.println("DDD    DD         IIIII        MM            MM");
+        System.out.println("DDDDDDDD          IIIII        MM            MM");
+        System.out.println("DDDDDD            IIIII        MM            MM");
+    }
+    
+    
+}
